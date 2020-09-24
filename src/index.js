d8eb8fe07fa68980b7889b3febccfd14f5797ed6
cody.koscielski <codykoscielski@gmail.com>
2020-09-24 17:04:29 -0400

    Set up conditional rendering

--- src/index.js
+++ src/index.js
@@ -1,13 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => {
-    window.navigator.geolocation.getCurrentPosition(
-        (position) => console.log(position),
-        (err) => console.log(err)
+class App extends React.Component {
+    constructor(props){
+        super(props);
+    
+        this.state = {
+            lat: null,
+            errorMessage: ''
+        };
 
-    );
-    return <div>Hi there!</div>;
-};
+
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => {
+                this.setState({ lat: position.coords.latitude });
+            },
+            (err) => {
+                this.setState({ errorMessage: err.message });
+            }
+        );
+    }
+
+    render() {
+        if(this.state.errorMessage && !this.state.lat) {
+            return <div> { this.state.errorMessage } </div>
+        }
+
+        if(!this.state.errorMessage && this.state.lat) {
+            return <div> latitude: { this.state.lat } </div>
+        }
+
+        return <div> Loading... </div>
+    }
+}
 
 ReactDOM.render(<App />, document.querySelector('#root'));
